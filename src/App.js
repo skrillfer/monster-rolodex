4826b43cc7f6d5686254a0622e1ebec4fe063097
fernando <rluis4490@gmail.com>
2019-07-31 16:27:06 -0600

    filter monster, using onChange event in input search

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
-      monsters:[]
+      monsters:[],
+      searchField : ''
     }
   }
   componentDidMount(){
@@ -16,9 +17,16 @@ class App extends Component{
   }
 
   render(){
+    const {monsters,searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster=>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <input type='search' 
+               placeholder='search monsters'
+               onChange={e=>this.setState({searchField:e.target.value})}></input>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
