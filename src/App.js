0f5d317d1a5183eb2645f29660522720c75ef4e9
fernando <rluis4490@gmail.com>
2019-07-31 16:54:44 -0600

    create a reusable component from input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component{
@@ -23,10 +25,11 @@ class App extends Component{
       );
     return (
       <div className="App">
-        <input type='search' 
-               placeholder='search monsters'
-               onChange={e=>this.setState({searchField:e.target.value})}></input>
-        <CardList monsters={filteredMonsters}></CardList>
+        <SearchBox 
+            placeholder='search monsters'
+            handleChange={e=>this.setState({searchField:e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
