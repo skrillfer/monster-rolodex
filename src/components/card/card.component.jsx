cafc5f4848ea98b8d89e7e1efbd1c18b790acc68
fernando <rluis4490@gmail.com>
2019-07-31 15:09:29 -0600

    add img to card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,12 @@ import './card.styles.css';
 
 export const Card = (props)=>{
     return <div className="card-container">
-                <h1>{props.monster.name}</h1>
+                <img 
+                    alt="monster" 
+                    src={'https://robohash.org/'+props.monster.id+'?set=set2&size=180x180'}>
+                </img>
+                <h2>{props.monster.name}</h2>
+                <p>{props.monster.email}</p>
             </div>
             
 }
