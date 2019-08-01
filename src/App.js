5bb3d64cffca9038ae742d5166060e85e984976a
fernando <rluis4490@gmail.com>
2019-08-01 08:16:01 -0600

    Exercise: Event Binding

--- src/App.js
+++ src/App.js
@@ -7,37 +7,35 @@ import './App.css';
 class App extends Component{
   constructor(props){
     super(props);
-    this.state={
-      monsters:[],
-      searchField : ''
-    }
-  }
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users    =>this.setState({monsters:users}))
-  }
-  //the arrow functions not needd be declared in constructor, because
-  //automatically when is compiled or interpreted set this component as scope from arrow function
-  //and we carefree for this: this.namefunction = this.namefunction.bin(this); 
-  handleChange= e =>{
-    this.setState({searchField:e.target.value})
+    this.handleClick2 = this.handleClick1.bind(this);
   }
 
-  render(){
-    const {monsters,searchField} = this.state;
-    const filteredMonsters = monsters.filter(monster=>
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
-    return (
-      <div className="App">
-        <SearchBox 
-            placeholder='search monsters'
-            handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
+  handleClick1(){
+    console.log('button 1 clicked');
+    console.log(this);
   }
+  
+  handleClick3 = () =>{console.log('button 3 clicked'+this);console.log(this);}
+  /*
+    btn1= when first render , the function handleClick is called  and not assigned to onClick event
+    ------------------------------------------------------------
+    btn1 = in btn1 console.log(this) print the context from app
+    btn2 = in btn2 console.log(this) print undefined, because handleClick1 not set bind in constructor
+    btn3 = this context is for this App, because in the constructor set handleClick2 = this.handleClic1.bin(this)
+    btn4 = this context this is set automatically in arrow fuctions, not neccesary set in constructor
+
+    https://reactjs.org/docs/handling-events.html
+  */
+  render(){
+    return (
+      <React.Fragment>
+        <button id="btn1" onClick={this.handleClick1()}>click1</button>
+        <button id="btn2" onClick={this.handleClick1}>click2</button>
+        <button id="btn3" onClick={this.handleClick2}>click3</button>
+        <button id="btn4" onClick={this.handleClick3}>click4</button>
+      </React.Fragment>
+    )
+  }
+  
 }
 export default App;
