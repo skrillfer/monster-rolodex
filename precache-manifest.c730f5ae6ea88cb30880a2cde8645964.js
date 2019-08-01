16c797d684133d9dca67a9c545e128ca037c061c
fernando <rluis4490@gmail.com>
2019-08-01 09:24:00 -0600

    Updates

---
+++ precache-manifest.c730f5ae6ea88cb30880a2cde8645964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000aeefa7e2b1bdf20df61089922ed77",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ef2d81a1ed3262186574",
+    "url": "/monster-rolodex/static/css/main.0c0c5487.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ef2d81a1ed3262186574",
+    "url": "/monster-rolodex/static/js/main.a63f4f7c.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
